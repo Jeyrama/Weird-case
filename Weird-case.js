6073a7ce1b9b1a4c956d5bddd8ca8abf89d3d27b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-12 21:28:02 -0800

    Update Weird-case.js

--- Weird-case.js
+++ Weird-case.js
@@ -16,3 +16,11 @@ Examples:
 
 
 // Solution
+
+function toWeirdCase(string){
+  return string.split(' ').map(function(word){
+    return word.split('').map(function(letter, index){
+      return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()
+    }).join('');
+  }).join(' ');
+}
